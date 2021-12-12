764ddfbef6611fa122716794d26196caddd4ef2b
samueldsm <sammuel980413@gmail.com>
2021-12-12 15:37:30 -0500

    Subiendo mi proyecto

---
+++ js/funciones.js
@@ -0,0 +1,18 @@
+function go() {
+
+    if (document.form.sistemaseguridadusuario.value == 'usuario' && document.form.sistemaseguridadcontrasenna.value == 'usuario') {
+        document.form.submit();
+       // falta preventDefault();
+        return false;
+
+    }
+    
+    
+    else {
+        alert("Por favor ingrese, un nombre de usuario y contraseña correctos.");
+       // falta preventDefault();
+        return false;
+
+    }
+}
+
