8690c7cebdcddf4e1752814a2eaabc6fddbf9305
samueldsm <sammuel980413@gmail.com>
2021-12-13 22:01:01 -0500

    Subiendo mejoras

--- js/funciones.js
+++ js/funciones.js
@@ -8,7 +8,21 @@ function go() {
     }
     
     
-    else {
+    else if (document.form.sistemaseguridadusuario.value == 'usuario' && document.form.sistemaseguridadcontrasenna.value == 'usuario') {
+        document.form.submit();
+       // falta preventDefault();
+        return false;
+
+    }
+    
+    else if (document.form.sistemaseguridadusuario.value == 'usuario' && document.form.sistemaseguridadcontrasenna.value == 'usuario') {
+        document.form.submit();
+       // falta preventDefault();
+        return false;
+
+    }
+    
+    else{
         alert("Por favor ingrese, un nombre de usuario y contraseña correctos.");
        // falta preventDefault();
         return false;
---
+++ js/my_jquery_functions.js
@@ -0,0 +1,63 @@
+
+
+//Autenticar usuario//
+
+$(document).ready(function(){
+
+/*
+  
+    /*$("login").click(function(){
+        //alert("Value: " + $("#sistemaseguridadusuario").val());
+        //alert("Value: " + $("#sistemaseguridadcontrasenna").val());
+
+      });
+
+     
+            $("#login").submit(function (e) { 
+              e.preventDefault();
+              
+              
+            });
+           
+        
+
+
+
+    $("p").click(function(){
+        // action goes here!!
+        $("p").hide(100, function () {
+           $("p").hide(); 
+        });
+      });
+
+*/
+
+       /* $("#delete").click(function(){
+
+          $("tbody tr td").empty();
+        });
+
+        
+
+          $('#logout').on('click', function(e){
+
+            e.preventDefault();
+            $('#logout').load($(this).attr('href'));
+          });
+        
+
+*/
+     
+
+    
+
+
+
+
+
+
+
+/*alert("Jquery funcionando")*/
+
+
+  });
